90f2f899c76825ce93880d8bcf34419f2843213c
deviceDmc <device.dmc@gmail.com>
2025-06-07 18:05:32 +0200

    Update streamlit_app.py
    error on col search_on

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ cnx= st.connection("snowflake")
 session = cnx.session()
 
 #conert de snowpark dataframe to pandas dataframe so we can use de LOC funciton
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col (SEARCH_ON))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col(SEARCH_ON))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
